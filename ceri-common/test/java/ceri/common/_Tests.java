aaa6857a55bb523a5d30a15e6904fa40cb4264a1
Ceri Jerome <cerijerome@gmail.com>
2012-12-30 18:27:25 -0800

    tidy up

--- ceri-common/test/java/ceri/common/_Tests.java
+++ ceri-common/test/java/ceri/common/_Tests.java
@@ -12,7 +12,6 @@ import ceri.common.comparator.ComparatorSequenceBehavior;
 import ceri.common.comparator.ComparatorsTest;
 import ceri.common.comparator.EnumComparatorsTest;
 import ceri.common.comparator.ReverseComparatorBehavior;
-import ceri.common.date.ImmutableDate;
 import ceri.common.date.ImmutableDateBehavior;
 import ceri.common.date.MultiDateParserBehavior;
 import ceri.common.io.BlockingBufferStreamBehavior;
