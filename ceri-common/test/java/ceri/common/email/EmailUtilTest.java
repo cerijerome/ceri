e6515339bb9dae586b8f5239257b040a25fb3adc
cerijerome <cerijerome@gmail.com>
2013-12-31 12:04:56 -0800

    Added email test

---
+++ ceri-common/test/java/ceri/common/email/EmailUtilTest.java
@@ -0,0 +1,28 @@
+package ceri.common.email;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class EmailUtilTest {
+
+	@Test
+	public void testValidEmails() {
+		assertTrue(EmailUtil.isValid("a@y.zz"));
+		assertTrue(EmailUtil.isValid("a.b@y.zz"));
+		assertTrue(EmailUtil.isValid("a@x.y.zz"));
+	}
+
+	@Test
+	public void testInvalidEmails() {
+		assertFalse(EmailUtil.isValid("@zzz"));
+		assertFalse(EmailUtil.isValid("yyy"));
+		assertFalse(EmailUtil.isValid("yyy@"));
+		assertFalse(EmailUtil.isValid("a@z"));
+		assertFalse(EmailUtil.isValid("a@z."));
+		assertFalse(EmailUtil.isValid("a@yy.z"));
+		assertFalse(EmailUtil.isValid("a@y.z."));
+		assertFalse(EmailUtil.isValid("a@x.y.z"));
+	}
+
+}
