a71f8d203b39c29d24ea2af41baaf0e7926e2af3
cerijerome <cerijerome@gmail.com>
2014-01-12 19:47:50 -0800

    Tidy up 2

---
+++ ceri-common/src/main/java/ceri/common/test/RegexMatcher.java
@@ -0,0 +1,24 @@
+package ceri.common.test;
+
+import java.util.regex.Pattern;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+public class RegexMatcher<T> extends BaseMatcher<T> {
+	private final Pattern pattern;
+	
+	public RegexMatcher(String regex) {
+		pattern = Pattern.compile(regex);
+	}
+	
+	@Override
+	public void describeTo(Description description) {
+		description.appendText("matches regex \"" + pattern.pattern() + "\"");
+	}
+	
+	@Override
+	public boolean matches(Object item) {
+		return pattern.matcher(item.toString()).matches();
+	}
+	
+}
