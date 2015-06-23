c7c097cf138911f37b2c0c23f9cab4dc24aadcd4
Ceri Jerome <cerijerome@gmail.com>
2015-06-22 18:16:44 -0700

    Added simple sample for MacOSX

--- ceri-x10/src/test/java/ceri/x10/SampleForMac.java
+++ ceri-x10/src/test/java/ceri/x10/SampleForMac.java
@@ -8,17 +8,19 @@ import ceri.x10.command.CommandFactory;
 import ceri.x10.util.X10Controller;
 
 /**
- * Example code for connecting to x10 devices via Mac
+ * Example code for connecting to x10 devices via MacOSX.
+ * Install PL2303 driver and rxtx lib shown in README.
  */
 public class SampleForMac {
 
 	public static void main(String[] args) throws IOException {
-		String commPort = "";
+		String commPort = "/dev/tty.usbserial";
 		try (Cm17aSerialConnector connector = new Cm17aSerialConnector(commPort)) {
 			try (X10Controller controller = new Cm17aController(connector, null)) {
 				controller.command(CommandFactory.on("A1"));
 				BasicUtil.delay(3000);
 				controller.command(CommandFactory.off("A1"));
+				BasicUtil.delay(3000);
 			}
 		}
 	}
