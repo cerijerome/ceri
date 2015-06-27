1d0855766d1042383d677a349bdc1aad7416583e
Ceri Jerome <cerijerome@gmail.com>
2015-06-27 02:00:59 -0700

    Filter is now compatible with Predicate and Streams

--- ceri-common/src/main/java/ceri/common/filter/Filter.java
+++ ceri-common/src/main/java/ceri/common/filter/Filter.java
@@ -1,10 +1,17 @@
 package ceri.common.filter;
 
+import java.util.function.Predicate;
+
 /**
  * Filter interface - returns true if the conditions are met.
  */
-public interface Filter<T> {
+public interface Filter<T> extends Predicate<T> {
 
 	boolean filter(T t);
 
+	@Override
+	default boolean test(T t) {
+		return filter(t);
+	}
+	
 }
