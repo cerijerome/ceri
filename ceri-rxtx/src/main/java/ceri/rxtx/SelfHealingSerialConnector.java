8e039f444517f4b7bbc0369a7aa819f3b06534a3
Ceri Jerome <cerijerome@gmail.com>
2015-10-18 02:27:03 -0700

    Added handler for comm port changing on unplug-replug event

--- ceri-rxtx/src/main/java/ceri/rxtx/SelfHealingSerialConnector.java
+++ ceri-rxtx/src/main/java/ceri/rxtx/SelfHealingSerialConnector.java
@@ -25,13 +25,14 @@ import ceri.log.util.LogUtil;
 
 /**
  * A self-healing serial connector. It will automatically reconnect if the cable is removed and
- * replaced.
+ * replaced. For USB-to-serial connectors, their device names can change after disconnecting and
+ * reconnecting. The commPortSupplier can contain logic to handle this situation.
  */
 public class SelfHealingSerialConnector extends LoopingExecutor {
 	private static final Logger logger = LogManager.getLogger();
 	public static final Pattern BROKEN_CONNECTION_REGEX = Pattern
 		.compile("(?i)device not configured");
-	private final String commPort;
+	private final CommPortSupplier commPortSupplier;
 	private final int baud;
 	private final int dataBits;
 	private final int stopBits;
@@ -46,8 +47,12 @@ public class SelfHealingSerialConnector extends LoopingExecutor {
 	private final BooleanCondition sync = BooleanCondition.create();
 	private volatile SerialPort serialPort;
 
+	public static interface CommPortSupplier {
+		String get() throws IOException;
+	}
+
 	public static class Builder {
-		final String commPort;
+		final CommPortSupplier commPortSupplier;
 		int baud = 9600;
 		int dataBits = SerialPort.DATABITS_8;
 		int stopBits = SerialPort.STOPBITS_1;
@@ -58,8 +63,8 @@ public class SelfHealingSerialConnector extends LoopingExecutor {
 		Predicate<IOException> brokenPredicate = e -> streamNotSet(e) ||
 			messageMatches(BROKEN_CONNECTION_REGEX, e);
 
-		Builder(String commPort) {
-			this.commPort = commPort;
+		Builder(CommPortSupplier commPortSupplier) {
+			this.commPortSupplier = commPortSupplier;
 		}
 
 		public Builder baud(int baud) {
@@ -108,11 +113,15 @@ public class SelfHealingSerialConnector extends LoopingExecutor {
 	}
 
 	public static Builder builder(String commPort) {
-		return new Builder(commPort);
+		return new Builder(() -> commPort);
+	}
+
+	public static Builder builder(CommPortSupplier commPortSupplier) {
+		return new Builder(commPortSupplier);
 	}
 
 	SelfHealingSerialConnector(Builder builder) {
-		commPort = builder.commPort;
+		commPortSupplier = builder.commPortSupplier;
 		baud = builder.baud;
 		stopBits = builder.stopBits;
 		dataBits = builder.dataBits;
@@ -158,11 +167,11 @@ public class SelfHealingSerialConnector extends LoopingExecutor {
 	public void setDTR(boolean state) {
 		serialPort.setDTR(state);
 	}
-	
+
 	public void setRTS(boolean state) {
 		serialPort.setRTS(state);
 	}
-	
+
 	@Override
 	public void close() {
 		super.close();
@@ -216,6 +225,7 @@ public class SelfHealingSerialConnector extends LoopingExecutor {
 
 	private void initSerialPort() throws IOException {
 		LogUtil.close(logger, serialPort);
+		String commPort = commPortSupplier.get();
 		serialPort = openSerialPort(commPort, connectionTimeoutMs);
 		in.setInputStream(serialPort.getInputStream());
 		out.setOutputStream(serialPort.getOutputStream());
