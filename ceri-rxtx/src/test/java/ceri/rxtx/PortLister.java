abeb8cc8dd2430bdb460f79dab8082441b0ad6c3
Ceri Jerome <cerijerome@gmail.com>
2015-12-18 02:53:47 -0800

    Added port lister test class. Added ubuntu install instructions.

---
+++ ceri-rxtx/src/test/java/ceri/rxtx/PortLister.java
@@ -0,0 +1,25 @@
+package ceri.rxtx;
+
+import gnu.io.CommPortIdentifier;
+import java.util.Enumeration;
+import ceri.common.collection.CollectionUtil;
+import ceri.common.util.BasicUtil;
+
+
+public class PortLister {
+
+	public static void main(String[] args) {
+		for (CommPortIdentifier id : identifiers()) {
+			String owner = id.getCurrentOwner();
+			if (owner == null) owner = "none";
+			System.out.printf("%s %d (%s)%n", id.getName(), id.getPortType(), owner);
+		}
+	}
+	
+	private static Iterable<CommPortIdentifier> identifiers() {
+		Enumeration<?> enumeration = CommPortIdentifier.getPortIdentifiers();
+		Enumeration<CommPortIdentifier> identifiers = BasicUtil.uncheckedCast(enumeration); 
+		return CollectionUtil.iterable(identifiers);
+	}
+	
+}
