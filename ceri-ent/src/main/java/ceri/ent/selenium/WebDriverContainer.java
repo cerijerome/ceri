e8a6e7509b5470f1d916641bcfcd7683687526b4
Ceri Jerome <cerijerome@gmail.com>
2016-10-29 21:25:22 -0700

    Refactored web driver helper to allow resets.

--- ceri-ent/src/main/java/ceri/ent/selenium/WebDriverContainer.java
+++ ceri-ent/src/main/java/ceri/ent/selenium/WebDriverContainer.java
@@ -3,6 +3,7 @@ package ceri.ent.selenium;
 import java.io.Closeable;
 import java.util.List;
 import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,42 +13,82 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ceri.common.math.MathUtil;
 import com.google.common.base.Function;
 
-public class WebDriverHelper implements Closeable {
+/**
+ * A container and support methods for selenium web drivers. Supports lifecycle management with
+ * create, reset, and close.
+ */
+public class WebDriverContainer implements Closeable {
 	private static final Logger logger = LogManager.getLogger();
 	private static final int TIMEOUT_MS_DEF = 5000;
+	private final Supplier<WebDriver> constructor;
 	private final int timeoutSec;
-	public final WebDriver driver;
-	public final JavascriptExecutor js;
+	private final boolean canReset;
+	private volatile WebDriver driver;
 
-	public WebDriverHelper(WebDriver driver) {
-		this(driver, TIMEOUT_MS_DEF);
+	public static WebDriverContainer simple(WebDriver driver) {
+		return simple(driver, TIMEOUT_MS_DEF);
 	}
 
-	public WebDriverHelper(WebDriver driver, long timeoutMs) {
-		this.driver = driver;
+	public static WebDriverContainer simple(WebDriver driver, long timeoutMs) {
+		return new WebDriverContainer(() -> driver, timeoutMs, false);
+	}
+
+	public WebDriverContainer(Supplier<WebDriver> constructor) {
+		this(constructor, TIMEOUT_MS_DEF);
+	}
+
+	public WebDriverContainer(Supplier<WebDriver> constructor, long timeoutMs) {
+		this(constructor, timeoutMs, true);
+	}
+
+	private WebDriverContainer(Supplier<WebDriver> constructor, long timeoutMs, boolean canReset) {
+		logger.info("{} started", getClass().getSimpleName());
+		this.constructor = constructor;
 		this.timeoutSec = (int) MathUtil.roundDiv(timeoutMs, 1000);
-		js = driver instanceof JavascriptExecutor ? (JavascriptExecutor) driver : null;
+		this.canReset = canReset;
+		driver = create(constructor);
+	}
+
+	public void reset() {
+		if (!canReset) throw new UnsupportedOperationException();
+		logger.info("Resetting web driver");
+		driver = create(constructor);
 	}
 
 	@Override
 	public void close() {
 		if (driver != null) driver.quit();
+		logger.info("{} stopped", getClass().getSimpleName());
+	}
+
+	/**
+	 * Never store the returned value as it will change after a reset.
+	 */
+	public WebDriver driver() {
+		return driver;
+	}
+
+	/**
+	 * Never store the returned value as it will change after a reset.
+	 */
+	public JavascriptExecutor js() {
+		if (driver instanceof JavascriptExecutor) return (JavascriptExecutor) driver;
+		return WebDriverUtil.nullJs();
 	}
 
 	public void get(String url) {
 		try {
-			driver.get(url);
+			driver().get(url);
 		} catch (TimeoutException e) {
 			logger.catching(Level.WARN, e);
-			driver.navigate().refresh();
+			driver().navigate().refresh();
 		}
 	}
-	
+
 	public WebElement waitForElement(String xPath) {
 		waitFor(() -> findElement(xPath) != null);
 		return findElement(xPath);
@@ -66,13 +107,9 @@ public class WebDriverHelper implements Closeable {
 		waitFor(driver -> test.getAsBoolean());
 	}
 
-	public static WebDriverHelper firefox(int timeoutMs) {
-		return new WebDriverHelper(new FirefoxDriver(), timeoutMs);
-	}
-
 	public WebElement findElement(By by) {
 		try {
-			return driver.findElement(by);
+			return driver().findElement(by);
 		} catch (NoSuchElementException e) {
 			return null;
 		}
@@ -83,7 +120,7 @@ public class WebDriverHelper implements Closeable {
 	}
 
 	public List<WebElement> findElementsById(String id) {
-		return driver.findElements(By.id(id));
+		return driver().findElements(By.id(id));
 	}
 
 	public WebElement findElement(String xPath) {
@@ -91,11 +128,15 @@ public class WebDriverHelper implements Closeable {
 	}
 
 	public List<WebElement> findElements(String xPath) {
-		return driver.findElements(By.xpath(xPath));
+		return driver().findElements(By.xpath(xPath));
 	}
 
 	public String pageSource() {
-		return driver.getPageSource();
+		return driver().getPageSource();
 	}
-	
+
+	private WebDriver create(Supplier<WebDriver> constructor) {
+		return constructor.get();
+	}
+
 }
---
+++ ceri-ent/src/main/java/ceri/ent/selenium/WebDriverUtil.java
@@ -0,0 +1,39 @@
+package ceri.ent.selenium;
+
+import org.openqa.selenium.JavascriptExecutor;
+
+public class WebDriverUtil {
+	public static final JavascriptExecutor NULL_JS_EXECUTOR = new JavascriptExecutor() {
+		@Override
+		public Object executeScript(String script, Object... args) {
+			return null;
+		}
+
+		@Override
+		public Object executeAsyncScript(String script, Object... args) {
+			return null;
+		}
+	};
+	public static final JavascriptExecutor UNSUPPORTED_JS_EXECUTOR = new JavascriptExecutor() {
+		@Override
+		public Object executeScript(String script, Object... args) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Object executeAsyncScript(String script, Object... args) {
+			throw new UnsupportedOperationException();
+		}
+	};
+
+	private WebDriverUtil() {}
+
+	public static JavascriptExecutor nullJs() {
+		return NULL_JS_EXECUTOR;	
+	}
+	
+	public static JavascriptExecutor unsupportedJs() {
+		return UNSUPPORTED_JS_EXECUTOR;	
+	}
+	
+}
