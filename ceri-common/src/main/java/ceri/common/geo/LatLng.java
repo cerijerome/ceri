e79704779553ca116f19096bc4f96b779c8e80a2
Ceri Jerome <cerijerome@gmail.com>
2017-09-22 22:23:36 -0700

    Moved LatLng to common

---
+++ ceri-common/src/main/java/ceri/common/geo/LatLng.java
@@ -0,0 +1,34 @@
+package ceri.common.geo;
+
+import ceri.common.util.EqualsUtil;
+import ceri.common.util.HashCoder;
+
+public class LatLng {
+	public final double latitude;
+	public final double longitude;
+
+	public LatLng(double lat, double lng) {
+		this.latitude = lat;
+		this.longitude = lng;
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCoder.hash(latitude, longitude);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (!(obj instanceof LatLng)) return false;
+		LatLng other = (LatLng) obj;
+		return EqualsUtil.equals(latitude, other.latitude) &&
+			EqualsUtil.equals(longitude, other.longitude);
+	}
+
+	@Override
+	public String toString() {
+		return "(" + latitude + "," + longitude + ")";
+	}
+
+}
