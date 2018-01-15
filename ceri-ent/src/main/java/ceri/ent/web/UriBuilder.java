16d76be629e7281b041f78bd9e89ff2589bdb156
Ceri Jerome <cerijerome@gmail.com>
2018-01-14 23:57:03 -0800

    Added wrapper for URIBuilder

---
+++ ceri-ent/src/main/java/ceri/ent/web/UriBuilder.java
@@ -0,0 +1,36 @@
+package ceri.ent.web;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.http.client.utils.URIBuilder;
+import ceri.common.function.ExceptionSupplier;
+
+public class UriBuilder {
+	private final URIBuilder builder;
+	
+	public static UriBuilder of(String uri) {
+		return new UriBuilder(wrap(() -> new URIBuilder(uri)));
+	}
+	
+	private UriBuilder(URIBuilder builder) {
+		this.builder = builder;
+	}
+	
+	public UriBuilder param(String name, Object value) {
+		if (value != null) builder.addParameter(name, String.valueOf(value));
+		return this;
+	}
+	
+	public URI build() {
+		return wrap(() -> builder.build());
+	}
+	
+	private static <T> T wrap(ExceptionSupplier<URISyntaxException, T> supplier) {
+		try {
+			return supplier.get();
+		} catch (URISyntaxException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+}
