bad50c6f8ddb82e64f917f1accb64b860ca7a838
Ceri Jerome <cerijerome@gmail.com>
2021-02-26 20:57:15 -0800

    Refactor of colors

--- ceri-ent/src/main/java/ceri/ent/json/adapter/VectorAdapter.java
+++ ceri-ent/src/main/java/ceri/ent/json/adapter/VectorAdapter.java
@@ -3,19 +3,19 @@ package ceri.ent.json.adapter;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonSerializationContext;
-import ceri.common.math.Vector;
+import ceri.common.math.Matrix;
 
 public class VectorAdapter {
-	public static final JsonSerialAdapter<Vector> INSTANCE =
-		JsonSerialAdapter.of(Vector.class, (json, typeOfT, context) -> deserialize(json, context),
+	public static final JsonSerialAdapter<Matrix> INSTANCE =
+		JsonSerialAdapter.of(Matrix.class, (json, typeOfT, context) -> deserialize(json, context),
 			(value, typeOfT, context) -> serialize(value, context));
 
-	private static Vector deserialize(JsonElement json, JsonDeserializationContext context) {
+	private static Matrix deserialize(JsonElement json, JsonDeserializationContext context) {
 		double[] values = context.deserialize(json, double[].class);
-		return Vector.of(values);
+		return Matrix.vector(values);
 	}
 
-	private static JsonElement serialize(Vector value, JsonSerializationContext context) {
+	private static JsonElement serialize(Matrix value, JsonSerializationContext context) {
 		return context.serialize(value.values());
 	}
 
