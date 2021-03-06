d007712ab6a68de02096104c2b2f90ea755445ca
Ceri Jerome <cerijerome@gmail.com>
2021-03-05 23:13:50 -0800

    Added missing file

---
+++ ceri-common/src/test/java/ceri/common/color/ColorUtilTest.java
@@ -0,0 +1,263 @@
+package ceri.common.color;
+
+import static ceri.common.color.ColorTestUtil.assertColor;
+import static ceri.common.color.ColorTestUtil.assertHsb;
+import static ceri.common.color.ColorUtil.color;
+import static ceri.common.test.AssertUtil.assertApprox;
+import static ceri.common.test.AssertUtil.assertArray;
+import static ceri.common.test.AssertUtil.assertEquals;
+import static ceri.common.test.AssertUtil.assertIterable;
+import static ceri.common.test.AssertUtil.assertStream;
+import static ceri.common.test.AssertUtil.assertThrown;
+import java.awt.Color;
+import java.util.stream.IntStream;
+import org.junit.Test;
+
+public class ColorUtilTest {
+
+	@Test
+	public void testApplyAlphaArgb() {
+		assertEquals(ColorUtil.applyAlphaArgb(0x00802040), 0xff000000);
+		assertEquals(ColorUtil.applyAlphaArgb(0x80802040), 0xff401020);
+		assertEquals(ColorUtil.applyAlphaArgb(0xff802040), 0xff802040);
+	}
+
+	@Test
+	public void testRgb() {
+		assertEquals(ColorUtil.rgb(0x98765432), 0x765432);
+		assertEquals(ColorUtil.rgb(color(0x98765432)), 0x765432);
+	}
+
+	@Test
+	public void testArgbFromText() {
+		assertEquals(ColorUtil.argb("test"), null);
+		assertEquals(ColorUtil.argb("clear"), 0);
+		assertEquals(ColorUtil.argb("aquamarine"), Colors.aquamarine.argb);
+		assertEquals(ColorUtil.argb("#fed"), 0xffffeedd);
+		assertEquals(ColorUtil.argb("0xfed"), 0xff000fed);
+		assertEquals(ColorUtil.argb("#fedcba98"), 0xfedcba98);
+	}
+
+	@Test
+	public void testValidArgbFromText() {
+		assertThrown(() -> ColorUtil.validArgb("test"));
+		assertEquals(ColorUtil.validArgb("aquamarine"), Colors.aquamarine.argb);
+	}
+
+	@Test
+	public void testGrayArgb() {
+		assertEquals(ColorUtil.grayArgb(0), 0xff000000);
+		assertEquals(ColorUtil.grayArgb(0xab), 0xffababab);
+	}
+
+	@Test
+	public void testMaxArgb() {
+		assertEquals(ColorUtil.maxArgb(0x80408020), 0x8080ff40);
+		assertEquals(ColorUtil.maxArgb(0x8040ff20), 0x8040ff20);
+		assertEquals(ColorUtil.maxArgb(0x80000000), 0x80000000);
+	}
+
+	@Test
+	public void testRandomArgb() {
+		assertEquals(ColorUtil.randomArgb() & 0xff000000, 0xff000000);
+	}
+
+	@Test
+	public void testDimArgb() {
+		assertEquals(ColorUtil.dimArgb(0xff664422, 0.0), 0xff000000);
+		assertEquals(ColorUtil.dimArgb(0xff664422, 1.0), 0xff664422);
+		assertEquals(ColorUtil.dimArgb(0xff664422, 0.5), 0xff332211);
+		assertEquals(ColorUtil.dimArgb(0xff664422, 0.125), 0xff0d0904);
+		assertEquals(ColorUtil.dimArgb(0x88664422, 0.5), 0x88332211);
+		assertEquals(ColorUtil.dimArgb(0x88000000, 0.5), 0x88000000);
+	}
+
+	@Test
+	public void testScaleArgb() {
+		assertEquals(ColorUtil.scaleArgb(0xff224466, 0xff446622, 0), 0xff224466);
+		assertEquals(ColorUtil.scaleArgb(0xff224466, 0xff446622, 1), 0xff446622);
+		assertEquals(ColorUtil.scaleArgb(0xff224466, 0xff446622, 0.5), 0xff335544);
+	}
+
+	@Test
+	public void testScaleHsbArgb() {
+		assertEquals(ColorUtil.scaleHsbArgb(0xff804020, 0xff208040, 0), 0xff804020);
+		assertEquals(ColorUtil.scaleHsbArgb(0xff804020, 0xff208040, 0.25), 0xff807020);
+		assertEquals(ColorUtil.scaleHsbArgb(0xff804020, 0xff208040, 0.5), 0xff608020);
+		assertEquals(ColorUtil.scaleHsbArgb(0xff804020, 0xff208040, 0.75), 0xff308020);
+		assertEquals(ColorUtil.scaleHsbArgb(0xff804020, 0xff208040, 1), 0xff208040);
+	}
+
+	@Test
+	public void testAlpha() {
+		assertColor(ColorUtil.alpha(0x80, Color.yellow), 0x80ffff00);
+	}
+
+	@Test
+	public void testApplyAlpha() {
+		assertColor(ColorUtil.applyAlpha(color(0x00802040)), 0xff000000);
+		assertColor(ColorUtil.applyAlpha(color(0x80802040)), 0xff401020);
+		assertColor(ColorUtil.applyAlpha(color(0xff802040)), 0xff802040);
+	}
+
+	@Test
+	public void testColorFromComponents() {
+		assertColor(color(0x80, 0x70, 0xff, 0), 0x8070ff00);
+	}
+
+	@Test
+	public void testColorFromText() {
+		assertColor(color("test"), null);
+		assertColor(color("clear"), 0);
+		assertColor(color("aquamarine"), Colors.aquamarine.argb);
+		assertColor(color("#fed"), 0xffffeedd);
+		assertColor(color("0xfed"), 0xff000fed);
+		assertColor(color("#fedcba98"), 0xfedcba98);
+	}
+
+	@Test
+	public void testValidColorFromText() {
+		assertThrown(() -> ColorUtil.validColor("test"));
+		assertColor(ColorUtil.validColor("aquamarine"), Colors.aquamarine.argb);
+	}
+
+	@Test
+	public void testGray() {
+		assertColor(ColorUtil.gray(0), 0xff000000);
+		assertColor(ColorUtil.gray(0xab), 0xffababab);
+	}
+
+	@Test
+	public void testMax() {
+		assertColor(ColorUtil.max(color(0x80408020)), 0x8080ff40);
+		assertColor(ColorUtil.max(color(0x8040ff20)), 0x8040ff20);
+		assertColor(ColorUtil.max(color(0x80000000)), 0x80000000);
+	}
+
+	@Test
+	public void testRandom() {
+		assertEquals(ColorUtil.random().getRGB() & 0xff000000, 0xff000000);
+	}
+
+	@Test
+	public void testDim() {
+		assertColor(ColorUtil.dim(color(0xff664422), 0.0), 0xff000000);
+		assertColor(ColorUtil.dim(color(0xff664422), 1.0), 0xff664422);
+		assertColor(ColorUtil.dim(color(0xff664422), 0.5), 0xff332211);
+		assertColor(ColorUtil.dim(color(0xff664422), 0.125), 0xff0d0904);
+		assertColor(ColorUtil.dim(color(0x88664422), 0.5), 0x88332211);
+		assertColor(ColorUtil.dim(color(0x88000000), 0.5), 0x88000000);
+	}
+
+	@Test
+	public void testScale() {
+		assertColor(ColorUtil.scale(color(0xff224466), color(0xff446622), 0), 0xff224466);
+		assertColor(ColorUtil.scale(color(0xff224466), color(0xff446622), 1), 0xff446622);
+		assertColor(ColorUtil.scale(color(0xff224466), color(0xff446622), 0.5), 0xff335544);
+	}
+
+	@Test
+	public void testScaleHsb() {
+		assertColor(ColorUtil.scaleHsb(color(0xff804020), color(0xff208040), 0), 0xff804020);
+		assertColor(ColorUtil.scaleHsb(color(0xff804020), color(0xff208040), 0.25), 0xff807020);
+		assertColor(ColorUtil.scaleHsb(color(0xff804020), color(0xff208040), 0.5), 0xff608020);
+		assertColor(ColorUtil.scaleHsb(color(0xff804020), color(0xff208040), 0.75), 0xff308020);
+		assertColor(ColorUtil.scaleHsb(color(0xff804020), color(0xff208040), 1), 0xff208040);
+	}
+
+	@Test
+	public void testScaleHsbColors() {
+		assertHsb(ColorUtil.scaleHsb(HsbColor.of(0.5, 0.8, 0.4), HsbColor.of(0.9, 0.4, 0.6), 0.0),
+			0.5, 0.8, 0.4);
+		assertHsb(ColorUtil.scaleHsb(HsbColor.of(0.5, 0.8, 0.4), HsbColor.of(0.9, 0.4, 0.6), 0.5),
+			0.7, 0.6, 0.5);
+		assertHsb(ColorUtil.scaleHsb(HsbColor.of(0.5, 0.8, 0.4), HsbColor.of(0.9, 0.4, 0.6), 1.0),
+			0.9, 0.4, 0.6);
+	}
+
+	@Test
+	public void testComponentAccess() {
+		assertEquals(ColorUtil.a(color(0x98765432)), 0x98);
+		assertEquals(ColorUtil.r(color(0x98765432)), 0x76);
+		assertEquals(ColorUtil.g(color(0x98765432)), 0x54);
+		assertEquals(ColorUtil.b(color(0x98765432)), 0x32);
+	}
+
+	@Test
+	public void testScaleHue() {
+		assertApprox(ColorUtil.scaleHue(0.9, 0.3, 0.0), 0.9);
+		assertApprox(ColorUtil.scaleHue(0.9, 0.3, 0.5), 0.1);
+		assertApprox(ColorUtil.scaleHue(0.9, 0.3, 1.0), 0.3);
+	}
+
+	@Test
+	public void testScaleValue() {
+		assertEquals(ColorUtil.scaleValue(0xde, 0x51, 0.0), 0xde);
+		assertEquals(ColorUtil.scaleValue(0xde, 0x51, 0.5), 0x98);
+		assertEquals(ColorUtil.scaleValue(0xde, 0x51, 1.0), 0x51);
+	}
+
+	@Test
+	public void testScaleRatio() {
+		assertApprox(ColorUtil.scaleRatio(0.9, 0.3, 0.0), 0.9);
+		assertApprox(ColorUtil.scaleRatio(0.9, 0.3, 0.5), 0.6);
+		assertApprox(ColorUtil.scaleRatio(0.9, 0.3, 1.0), 0.3);
+	}
+
+	@Test
+	public void testToString() {
+		assertEquals(ColorUtil.toString(ColorUtil.clear), "#00000000(clear)");
+		assertEquals(ColorUtil.toString(Colors.aquamarine.color()), "#7fffd4(aquamarine)");
+		assertEquals(ColorUtil.toString(Colors.aquamarine.argb), "#7fffd4(aquamarine)");
+		assertEquals(ColorUtil.toString(0xffffeedd), "#ffeedd");
+	}
+
+	@Test
+	public void testName() {
+		assertEquals(ColorUtil.name(ColorUtil.clear), "clear");
+		assertEquals(ColorUtil.name(Colors.aquamarine.color()), "aquamarine");
+		assertEquals(ColorUtil.name(color(0x123456)), null);
+	}
+
+	@Test
+	public void testHex() {
+		assertEquals(ColorUtil.hex(color(0x123456)), "#00123456");
+		assertEquals(ColorUtil.hex(color(0xff123456)), "#123456");
+	}
+
+	@Test
+	public void testArgbsFromText() {
+		assertArray(ColorUtil.argbs("clear", "#def", "0x12345678"), 0, 0xffddeeff, 0x12345678);
+		assertThrown(() -> ColorUtil.argbs("test"));
+	}
+
+	@Test
+	public void testColorsFromText() {
+		assertArray(ColorUtil.colors("clear", "#def", "0x12345678"), ColorUtil.clear,
+			color(0xffddeeff), color(0x12345678));
+		assertThrown(() -> ColorUtil.colors("test"));
+	}
+
+	@Test
+	public void testArgbList() {
+		assertIterable(ColorUtil.argbList(IntStream.of(0, 0x12345678)), 0, 0x12345678);
+	}
+
+	@Test
+	public void testColorList() {
+		assertIterable(ColorUtil.colorList(IntStream.of(0, 0x12345678)), ColorUtil.clear,
+			color(0x12345678));
+	}
+
+	@Test
+	public void testArgbStream() {
+		assertStream(ColorUtil.rgbStream(0, 0x12345678), 0xff000000, 0xff345678);
+	}
+
+	@Test
+	public void testFadeStream() {
+		assertStream(ColorUtil.fadeStream(color(0x204060), color(0x406020), 4, Bias.NONE), 0x284850,
+			0x305040, 0x385830, 0x406020);
+	}
+
+}
